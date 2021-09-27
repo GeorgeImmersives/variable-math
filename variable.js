7dca3a4d1baf60029f5739a0da95465e59ad0f88
GeorgeImmersives <84101766+GeorgeImmersives@users.noreply.github.com>
2021-09-27 13:07:40 -0400

    init

---
+++ variable.js
@@ -0,0 +1,34 @@
+let age = 24;
+
+console.log(`You will be ${age+5} in 5 years!`);
+
+//--------------------------------------------------------
+
+let num1 = 243;
+let num2 = 314;
+let sum = num1 + num2;
+
+console.log(`${num1} and ${num2} adds up to ${sum}`);
+
+//--------------------------------------------------------
+
+const name = 'George';
+
+console.log(`Hello ${name}`);
+
+//--------------------------------------------------------
+
+let pos1 = 24;
+let pos2 = 4;
+
+console.log(`The remainder of ${pos1} divided by ${pos2} is ${pos1 % pos2}`);
+
+//--------------------------------------------------------
+
+let x1 = 24;
+let y1 = 12;
+let x2 = 6;
+let y2 = 12;
+let distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+console.log(`The distance between ${x1},${x2} and ${y1},${y2} is ${distance}`);
